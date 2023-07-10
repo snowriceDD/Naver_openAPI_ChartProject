c85a978502c62dd65c0c1ef06b15a63ea7b62287
snowriceDD <yoons100457@gmail.com>
2023-07-11 01:04:03 +0900

    "feat: data fetch success"

--- src/lib/hooks/useTrendData.ts
+++ src/lib/hooks/useTrendData.ts
@@ -1,57 +1,54 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 interface TrendData {
-  statusCode: number;
-  body: string;
+  startDate: string;
+  endDate: string;
+  timeUnit: string;
+  category: string;
+  keyword: string;
+  device: string;
+  gender: string;
+  ages: string[];
 }
 
-const useTrendData = () => {
-  const [trendData, setTrendData] = useState<TrendData | null>(null);
+const useTrendData = (reqParams: TrendData) => {
+  const [data, setData] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<any>(null);
+
 
   useEffect(() => {
     const fetchData = async () => {
       const client_id = import.meta.env.VITE_APP_N_CLIENT_ID;
       const client_secret = import.meta.env.VITE_APP_N_CLIENT_SECRET;
-
       const apiUrl = "http://localhost:3001/proxy";
 
-      const requestBody =  {
-        startDate: '2017-08-01',
-        endDate: '2017-09-30',
-        timeUnit: 'month',
-        keywords: ['키워드1', '키워드2'],
-        category: '50000000',
-        device: 'pc',
-        ages: ['20', '30'],
-        gender: 'f',
-      };
 
       try {
         console.log(apiUrl)
-        const response = await axios.post(apiUrl, requestBody, {
+        const response = await axios.post(apiUrl, reqParams, {
           headers: {
             'X-Naver-Client-Id': client_id,
             'X-Naver-Client-Secret': client_secret,
             'Content-Type': 'application/json',
           },
         });
-
-        setTrendData({
-          statusCode: response.status,
-          body: JSON.stringify(response.data),
-        });
-      } catch (error) {
-        console.error('Error fetching trend data:', error);
+        setData(response.data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchData();
-  }, []);
+  }, [reqParams]);
 
-  return trendData;
+  return [data, loading, error];
 };
 
 export default useTrendData;
--- src/pages/PagesShoppingInsight.tsx
+++ src/pages/PagesShoppingInsight.tsx
@@ -4,20 +4,27 @@ import { useState, useEffect } from "react";
 import useTrendData from "../lib/hooks/useTrendData";
 
 export function PagesShoppingInsight() {
-    const trendData = useTrendData();
+  const [params, setParams] = useState({
+    startDate: '2017-08-01', 
+    endDate: '2017-09-30', 
+    timeUnit: 'month', 
+    category: '50000000',
+    keyword: '정장',
+    device: '', 
+    gender: '',
+    ages: ['20', '30'], 
+  });
 
-    
-    return (
-        <>
-        <p>123</p>
-        {trendData ? (
-        <>
-          <p>Status Code: {trendData.statusCode}</p>
-          <pre>{trendData.body}</pre>
-        </>
-      ) : (
-        <p>Loading...</p>
-      )}
-        </>
-    )
+    const [data, loading, error] = useTrendData(params);
+
+    if (loading) return <p>데이터를 불러오는 중입니다...</p>;
+  if (error) return <p>데이터를 불러오는 데 실패했습니다.</p>;
+  if (!data) return null;
+
+  return (
+    <div>
+      <h1>데이터</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 }
